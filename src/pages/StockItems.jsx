a77a3760f7440dfc6223e70bacb4a834ac27dd71
LogSyn <barbaratessn@gmail.com>
2023-05-27 00:00:09 +0300

    Stock Items page layout

--- src/pages/StockItems.jsx
+++ src/pages/StockItems.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getMedicationStockItems, reset } from '../features/stock/stockSlice'
+import Spinner from '../components/Spinner'
+import { toast } from 'react-toastify'
 
 function StockItems() {
     //Access the global state
@@ -8,7 +10,34 @@ function StockItems() {
         (state) => state.stock
     )
 
-    useEffect(() => {}, [])
+    const dispatch = useDispatch()
+
+    //Fetch the stock items on page load
+    useEffect(() => {
+        dispatch(getMedicationStockItems)
+    }, [dispatch])
+
+    //Handle any potential errors
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+    }, [isError, message])
+
+    //Reset the state on unmount -
+    //Not sure why this is needed yet
+    useEffect(() => {
+        return () => {
+            if (isSuccess) {
+                dispatch(reset())
+            }
+        }
+    }, [isSuccess, dispatch])
+
+    if (isLoading) {
+        return <Spinner />
+    }
+
     return (
         <div className='stockItemsContainer'>
             <h1>Stock Items</h1>
@@ -18,6 +47,9 @@ function StockItems() {
                 <div>Expiry Date</div>
                 <div></div>
             </div>
+            {stockItems.length === 0
+                ? toast.info('No items in stock.')
+                : stockItems.map((item) => <StockItem key={item._id} />)}
         </div>
     )
 }
